e20a0282175325dbdbc7947452de2bad259a4556
LeeJongbin <ljb1905@ajou.ac.kr>
2017-12-19 23:36:45 +0900

    test

--- run.c
+++ run.c
@@ -15,12 +15,11 @@ p_meta find_meta(p_meta *last, size_t size) {
   switch(fit_flag){
     case FIRST_FIT:
     {
-     while(index) 
-      {
+     while(index)
+     {
         if(result != base) break;
         if(index->free && index->size>=size) result = index;
-        if(result->size > index->size) result = index;
-          if(index->next == NULL) result = index;
+        if(index->next == NULL) result = index;
         index = index->next;
       }
     }
@@ -65,25 +64,83 @@ if(base==0)
   if(size%4!=0) size += (4-size%4);
   int l = size + META_SIZE;
   p_meta tmp = find_meta(end,size);
+  if(tmp==-1 || (!tmp->next && (!tmp->free || tmp->free && tmp->size<size))){
+  //empty handling
   p_meta tmp2 = end;
   end += l;
   int ret = brk(end);
   if(ret == -1) return ; 
-  tmp2->free = 0;
-  tmp2->next = 0;
   tmp2->prev = tmp;
   tmp2->size = size;
   if(tmp!=-1) tmp->next = tmp2;
   tmp = tmp2;
+}
+else m_realloc(tmp->data,size);
   return tmp->data;
 }
 
 void m_free(void *ptr) {
-printf("ASdf");
+  p_meta now = ptr-META_SIZE;
+  now->free = 1;
+  if(now->next && now->next->free ==1) // next O
+  {
+    now->size = now->size+now->next->size + META_SIZE;
+    now->next = now->next->next;
+  }
+  if(now->prev!=-1 && now->prev->free ==1) // pre O
+  {
+    now = now ->prev;
+    now->size = now->size+now->next->size + META_SIZE;
+    now->next = now->next->next;
+  }
+  if(now->prev!=-1 && now->next==NULL)
+  {
+    end = end - now->size + META_SIZE;
+    now->prev->next = 0;
+  }
+  if(now->next==NULL && now->prev==NULL) end = base;
+  ptr=0;
 }
 
 void*
 m_realloc(void* ptr, size_t size)
 {
-printf("Asdf");
+  p_meta now = ptr-META_SIZE;
+  int flag = 0;
+  if(size%4!=0) size += (4-size%4);
+  if(now->size == size) return ptr; // don't need to realloc
+  if(now->size < size){
+    int chk = now->size + now->next->size;
+    if(now->next && now->next->free && chk + META_SIZE>=size){
+      now->size = now->size + now->next->size + META_SIZE;
+      now->next = now->next->next;
+      now->next->prev = now;
+      if(now->size-size < META_SIZE) // don't need to realloc
+        return ptr;
+      else
+        flag = 1;
+    }
+    else {
+      m_free(now->data);
+      void * start = m_malloc(size);
+      strcpy(start, ptr); 
+      return start;
+    }
+  }
+  else if(flag==0 && now->size-size < META_SIZE)
+    return ptr;
+  else
+   flag = 1;
+  if(flag)
+  {
+    p_meta tmp = (int)now + size + META_SIZE;
+    tmp->size = now->size - size - META_SIZE;
+    tmp->prev = now;
+    tmp->next = now->next;
+    now->size = size;
+    now->free = 0;
+    now->next = tmp;
+    m_free(tmp->data);
+    return now->data;
+  }
 }
